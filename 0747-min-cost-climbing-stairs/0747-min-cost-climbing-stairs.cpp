5960a1726fa48e651c94c022bc7250ea3de3e695
MAKINEEDI LAKSHMI NARAYANA <142885683+MAKINEEDI05@users.noreply.github.com>
2025-09-13 21:08:43 +0530

    Time: 0 ms (100%), Space: 17.8 MB (31.09%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int fun(int i,vector<int>& A,vector<int>& cost,int n){
+        if(i>=n){
+            return 0;
+        }
+        if(A[i]!=-1){
+            return A[i];
+        }
+        int a=fun(i+1,A,cost,n);
+        int b=fun(i+2,A,cost,n);
+        A[i]=cost[i]+min(a,b);
+        return A[i];
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int a=0,i=0,n=cost.size();
+        vector<int> A(n,-1);
+        // A[n-1]=cost[n-1];
+        // A[n-2]=cost[n-2];
+        a=fun(i,A,cost,n);
+        return min(A[0],A[1]);
+    }
+};

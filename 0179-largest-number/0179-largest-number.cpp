2906efdbcc71f205a761ecbf07b9be77f1bac7fb
MAKINEEDI LAKSHMI NARAYANA <142885683+MAKINEEDI05@users.noreply.github.com>
2025-10-11 11:26:08 +0530

    Time: 0 ms (100%), Space: 17.1 MB (42.34%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        int a=0,i=0,n=nums.size();
+        vector<string> A;
+        string AA="";
+        for(i=0;i<n;i++){
+            AA=to_string(nums[i]);
+            A.push_back(AA);
+        }
+        AA="";
+        sort(A.begin(),A.end(), [](string A,string B){return A+B>B+A;});
+        if(A[0]=="0"){
+            return "0";
+        }
+        for(i=0;i<n;i++){
+            AA+=A[i];
+        }
+        return AA;
+    }
+};

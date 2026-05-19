49fdd8ae6b631dbd09664187995a6011a3624669
MAKINEEDI LAKSHMI NARAYANA <142885683+MAKINEEDI05@users.noreply.github.com>
2026-05-19 11:51:27 +0530

    Time: 0 ms (100%), Space: 10.7 MB (42.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    int fun(int i,vector<int>& A,vector<int>& nums,int n){
-        if(i>=n){
-            return 0;
-        }
-        if(A[i]!=-1){
-            return A[i];
-        }
-        int left=nums[i]+fun(i+2,A,nums,n);
-        int right=fun(i+1,A,nums,n);
-        return A[i]=max(left,right);
+    int fun(vector<int>& nums,vector<int>& A,int i,int n){
+        if(i>=n) return 0;
+        if(A[i]!=-1) return A[i];
+        int left=nums[i]+fun(nums,A,i+2,n);
+        int right=fun(nums,A,i+1,n);
+        A[i]=max(left,right);
+        return A[i];
     }
     int rob(vector<int>& nums) {
-        int a=0,i=0,n=nums.size();
-        vector<int> A(n,0);
-        A[0]=nums[0];
-        if(n==1){
-            return nums[0];
-        }
-        A[1]=max(nums[0],nums[1]);
-        for(i=2;i<n;i++){
-            int take=nums[i]+A[i-2];
-            int nottake=A[i-1];
-            A[i]=max(take,nottake);
-        }
-        return A[n-1];
+        int a=0,n=nums.size();
+        vector<int> A(n,-1);
+        return fun(nums,A,0,n);
     }
 };

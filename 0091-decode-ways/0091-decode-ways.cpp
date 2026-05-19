bc49c856c68c4f90388885e7b4ab3234c7ac1797
MAKINEEDI LAKSHMI NARAYANA <142885683+MAKINEEDI05@users.noreply.github.com>
2026-05-19 14:50:46 +0530

    Time: 2 ms (22.51%), Space: 11.1 MB (6.92%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int dp[101];
-    int fun(string &s,int i,int n){
+    int fun(string s,vector<int>& A,int i,int n){
         if(i==n) return 1;
         if(i>n || s[i]=='0') return 0;
-        if(dp[i]!=-1) return dp[i];
-        int left=fun(s,i+1,n),right=0;
+        if(A[i]!=-1) return A[i];
+        int left=fun(s,A,i+1,n),right=0;
         if(s[i]<'2' || (s[i]<='2' && s[i+1]<='6') ){
-            right=fun(s,i+2,n);
+            right=fun(s,A,i+2,n);
         }
-        return dp[i]=left+right;
+        return A[i]=left+right;
     }
     int numDecodings(string s) {
-        int a=0,i=0,n=s.size();
-        memset(dp,-1,sizeof(dp));
-        return fun(s,0,n);
+        int a=0,i=0,n=s.size(); 
+        vector<int> A(n,-1);
+        return fun(s,A,0,n);
     }
 };
